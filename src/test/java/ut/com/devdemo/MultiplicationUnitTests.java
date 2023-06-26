342a9abe1ff0566659e6041715fe96255d5b6066
Decree Labs <88178632+decreelabs@users.noreply.github.com>
2023-06-26 11:52:19 +1000

    Initial plugin skeleton

---
+++ src/test/java/ut/com/devdemo/MultiplicationUnitTests.java
@@ -0,0 +1,31 @@
+package ut.com.devdemo;
+
+import org.junit.Test;
+import com.devdemo.api.MyPluginComponent;
+import com.devdemo.impl.MyPluginComponentImpl;
+
+import static org.junit.Assert.assertEquals;
+
+public class MultiplicationUnitTests
+{
+    @Test
+    public void testMultiplicationService()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+
+    @Test
+    public void testMultiplicationService2()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+
+    @Test
+    public void testMultiplicationService3()
+    {
+        Integer sum = 10;
+        assertEquals(10, sum.intValue());
+    }
+}
